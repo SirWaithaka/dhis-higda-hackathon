5f24dee05e7813e0504e2b51b77adc5a3b14ad41
craftsmon <craftsmon@outlook.com>
2017-07-12 19:10:07 +0300

    Removed pandas import

--- app/home/data.py
+++ app/home/data.py
@@ -1,5 +1,4 @@
 import requests
-import pandas as pd
 
 # dhis authentication
 auth = {
--- app/home/views.py
+++ app/home/views.py
@@ -1,4 +1,5 @@
-from flask import abort, render_template
+from flask import abort, render_template, redirect, request
+from flask_paginate import Pagination, get_page_parameter
 
 from . import home
 from .data import auth, getJson
@@ -11,6 +12,13 @@ def index():
     """
     return render_template('home/index.html', title = "Welcome")
 
+@home.route('/charts')
+def charts():
+    """
+    Render the charts template
+    """
+    return render_template("home/charts.html", title = "Charts")
+
 @home.route('/organisations')
 def organisations():
     """
@@ -20,11 +28,19 @@ def organisations():
     organisationUnitsUrl = "https://play.dhis2.org/release1/api/organisationUnits.json?paging=false"
     orgUnits = getJson(organisationUnitsUrl, auth)['organisationUnits'] # get a list of dictionaries
 
-    return render_template('home/organisations.html', title = "Organisation Units", organisation_units = orgUnits)
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+
+    pagination = Pagination(page=page, per_page=20, total=len(orgUnits), search=True, record_name="Organisation Units")
+
+    return render_template('home/organisations.html', organisation_units=orgUnits, title="Organisation Units", pagination=pagination)
+
+@home.route('/maps')
+def maps():
+    return redirect('/docs/python/workspace/safi_product/health.html')
 
 @home.route('/elements')
 def elements():
-    """
+    """/docs/python/workspace/safi_product/health.html
     Render the route for data elements
     """
     # pull data elements using requests
